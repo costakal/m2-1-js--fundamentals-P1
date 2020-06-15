3e10c90d3c57d2e451a4f4d1b11e13ac31079405
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-15 17:01:44 -0400

    exercise 6.4

--- workshop/exercise-6.4.js
+++ workshop/exercise-6.4.js
@@ -1,2 +1,5 @@
 // Q6.4
 // Write a loop that will print to the console all of the EVEN integers from 100 to 0.
+for (let number = 100; number > -1; number = number -2) {
+    console.log(number);
+}
