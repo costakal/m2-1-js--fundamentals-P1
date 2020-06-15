f4f27279c8a18375f6078dd54f20298e92442b01
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-15 16:37:35 -0400

    exercise 4

--- workshop/exercise-4.js
+++ workshop/exercise-4.js
@@ -5,6 +5,9 @@
 // eg:
 // 0, 1, 4, 9, 16, ...
 
-for (let number = 0; number < 12; number++) {
+
+
+for (let number = 0; number <= 12; number++) {
+  let square = (number * number);
   console.log('the square of ', number, ' is ', square);
 }
